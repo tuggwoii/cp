16f72b863be62a8e67a5a28394b39ea7bceca3cf
TuGG <tugg.solo@gmail.com>
2016-06-11 08:45:43 +0700

    cfghcfg

--- src/static/js/modules/accounts/services/accounts.js
+++ src/static/js/modules/accounts/services/accounts.js
@@ -12,16 +12,15 @@ module.factory('AccountService', ['$http', '$q', 'URLS', function ($http, $q, UR
         },
         logout: function () {
             return $q(function (resolve, reject) {
-                function handleSessionResponse(response) {
-                    if (!response.authResponse) {
-                        return;
+                FB.getLoginStatus(function (crets) {
+                    if (crets.authResponse) {
+                        FB.logout(function (response) {
+                            $http.post(URLS.model('accounts').logout).success(resolve).error(reject);
+                        });
+                    } else {
+                        $http.post(URLS.model('accounts').logout).success(resolve).error(reject);
                     }
-                    else {
-                        FB.logout(response.authResponse);
-                    }
-                    $http.post(URLS.model('accounts').logout).success(resolve).error(reject);
-                }
-                FB.logout(handleSessionResponse);
+                });
             });
         }
     };
